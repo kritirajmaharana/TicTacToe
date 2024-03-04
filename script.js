83ccb8e5952b0ad0b5ff295007a1c84b9e43cacb
kritirajmaharana <124510753+kritirajmaharana@users.noreply.github.com>
2024-03-04 11:49:30 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-let boxes = document.querySelectorAll('.box');
-let restBtn = document.getElementById('resetBtn')
+(function GameBoard() {
 
-let turn0 = true;
-const arr = [[],['','',''],['','','']]
+
+const box = document.querySelectorAll('.box');
+const rest = document.querySelector('#resetBtn');
+
+let iszero;
+
+const winningArr = 
+[[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2],
+];
+
+box.forEach(element => {
+   element.addEventListener('click', () => {
+    if(iszero){
+        element.textContent = "x"
+        iszero = false;
+        element.setAttribute("disabled", false)
+    }else{
+        element.textContent = "o"
+        iszero = true;
+        element.setAttribute("disabled", false)
+
+    }
+   }) 
+});
+})();
